afb8815223a6f51500475bfa441ef32f8b195c93
pgonzales7 <118759321+pgonzales7@users.noreply.github.com>
2022-11-21 13:40:35 -0500

    Add files via upload

---
+++ js/ej13.js
@@ -0,0 +1,27 @@
+// Sacar el promedio de 3 notas
+
+let num1 = parseInt (prompt("Ingrese nota 01", ));
+let num2 = parseInt (prompt("Ingrese nota 02", ));
+let num3 = parseInt (prompt("Ingrese nota 03", ));
+let promedio = (num1+num2+num3)/3;
+document.write(`El promedio es: ${promedio}`);
+
+/*
+// Leer el sueldo de 3 empleados y aplicarles un aumento del 10%, 12% y 15% respectivamente. Mostrar reporte con los nuevos resultados
+
+let sueldo1 = parseFloat (prompt("Ingrese sueldo 1", )) * 0.10;
+let sueldo2 = parseFloat (prompt("Ingrese sueldo 2", )) * 0.12;
+let sueldo3 = parseFloat (prompt("Ingrese sueldo 3", )) * 0.15;
+
+document.write("EL aumento del Trabajador 01 es de " + sueldo1);
+document.write(" EL aumento del Trabajador 02 es de " + sueldo2);
+document.write(" EL aumento del Trabajador 03 es de " + sueldo3);
+
+
+// Un alumno desea saber cuál es su calificación final en un curso, dicha calificación se compone de tres exámenes cuyos pesos son del 30%, 30% y 40%
+let ex1 = parseInt (prompt("Ingrese nota 01", ))* 0.30;
+let ex2 = parseInt (prompt("Ingrese nota 02", ))* 0.30;
+let ex3 = parseInt (prompt("Ingrese nota 03", ))* 0.40;
+let final = (ex1+ex2+ex3);
+document.write(`Su calificación final es: ${final}`);
+*/
